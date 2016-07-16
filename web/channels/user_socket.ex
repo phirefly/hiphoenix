fcd86b9ac9bf78178a46c47afd9262fa46b7f8bf
Raphael Villas <raphael.villas@gmail.com>
2016-07-16 14:58:14 -0500

    Added Procfile and update timeout info

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Hiphoenix.UserSocket do
   # channel "room:*", Hiphoenix.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
